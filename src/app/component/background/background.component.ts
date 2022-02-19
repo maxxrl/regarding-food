43ee55f3ff5397cf96a2435a3bedef93db763952
maxxrl <m.wesener@gmx.de>
2022-02-19 15:34:59 +0100

    Added background-component / styling

--- src/app/component/background/background.component.ts
+++ src/app/component/background/background.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
-  styleUrls: ['./background.component.scss']
+  styleUrls: ['./background.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BackgroundComponent implements OnInit {
 
