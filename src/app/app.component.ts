4d80404216fba1ed1d397160198562dab7e65273
maxxrl <m.wesener@gmx.de>
2022-02-20 01:33:01 +0100

    Added routing to views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router, RouterOutlet} from "@angular/router";
 import {slideInAnimation} from "./animations";
 
@@ -22,10 +22,21 @@ export class AppComponent {
     return outlet?.activatedRouteData?.['animation'];
   }
 
-  navigateToRoute(): void {
+  navigateToList(): void {
     this.router.navigate(['list-food']);
   }
 
+  navigateToPicker(): void {
+    this.router.navigate(['pick-food']);
+  }
+
+  public isFoodPickerRoute(): boolean {
+    return this.router.url === "/pick-food";
+  }
+
+  public isFoodListRoute(): boolean {
+    return this.router.url === "/list-food";
+  }
 
 }
 
